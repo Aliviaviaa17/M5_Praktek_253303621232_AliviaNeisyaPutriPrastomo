8409fd66294e68317f13f5ef647ad6b96cc36710
Aliviaviaa17 <kziahaa@gmail.com>
2026-04-16 21:14:06 +0700

    Update CreditCard.java

--- CreditCard.java
+++ CreditCard.java
@@ -1,6 +1,4 @@
 public class CreditCard extends Payment {
-
-    // Override method process
     @Override
     public void process(int amount) {
         System.out.println("Menghubungi Bank... Memotong Kartu Kredit Rp " + amount + "... Suksess!");
