7ea5d18f72780a245d6b5bddd1b2d805b798b911
Aliviaviaa17 <kziahaa@gmail.com>
2026-04-16 21:14:16 +0700

    Update EWallet.java

--- EWallet.java
+++ EWallet.java
@@ -1,12 +1,10 @@
 public class EWallet extends Payment {
-
-    // Override method process
     @Override
     public void process(int amount) {
         System.out.println("Memotong saldo E-Wallet sebesar Rp " + amount + "... Suksess!");
     }
 
-    // Method khusus
+    // Method khusus E-Wallet
     public void inputPin() {
         System.out.println("Memasukkan 6 digit PIN E-Wallet...");
     }
